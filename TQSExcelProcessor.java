25072a955625c85a44d40c803b9ad2071b71ae92
Manteiga <rafael.manteiga@hotmail.com>
2024-11-01 00:00:13 -0300

    Add files via upload

--- TQSExcelProcessor.java
+++ TQSExcelProcessor.java
@@ -60,10 +60,10 @@ public class TQSExcelProcessor extends JFrame {
             outputArea.append("\nArquivo vazio ou não encontrado.");
             return;
         }
-
+    
         boolean quantitativosEncontrados = false;
         List<String> linhasEncontradas = new ArrayList<>();
-
+    
         for (int i = 0; i < linhas.size(); i++) {
             String linha = linhas.get(i);
             if (linha.contains("Quantitativos")) {
@@ -77,22 +77,27 @@ public class TQSExcelProcessor extends JFrame {
                 break;
             }
         }
-
+    
         if (quantitativosEncontrados) {
             List<List<String>> palavras = new ArrayList<>();
             for (String linha : linhasEncontradas) {
                 List<String> palavrasLinha = List.of(linha.split("\\s+"));
                 palavras.add(palavrasLinha);
             }
-            salvarComoXLS(palavras);
+            // Aqui você precisa passar 'file' e 'palavras' para o método salvarComoXLS
+            salvarComoXLS(palavras, file); // Chamada corrigida
         } else {
             outputArea.append("\nA palavra 'Quantitativos' não foi encontrada.");
         }
-    }
+    }    
 
-    private void salvarComoXLS(List<List<String>> palavras) {
-        File file = new File("palavras_extraidas.xls");
-
+    private void salvarComoXLS(List<List<String>> palavras, File selectedFile) {
+        // Extrai o nome do arquivo original sem a extensão
+        String nomeArquivo = selectedFile.getName();
+        // Define o novo nome do arquivo com a extensão .xls
+        String novoNomeArquivo = nomeArquivo.substring(0, nomeArquivo.lastIndexOf('.')) + "_extraidas.xls";
+        File file = new File(novoNomeArquivo);
+    
         try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"))) {
             writer.write("<?xml version=\"1.0\"?>\n");
             writer.write("<?mso-application progid=\"Excel.Sheet\"?>\n");
@@ -103,7 +108,7 @@ public class TQSExcelProcessor extends JFrame {
             writer.write(" xmlns:html=\"http://www.w3.org/TR/REC-html40\">\n");
             writer.write("<Worksheet ss:Name=\"Sheet1\">\n");
             writer.write("<Table>\n");
-
+    
             for (List<String> linha : palavras) {
                 writer.write("  <Row>\n");
                 for (String palavra : linha) {
@@ -111,16 +116,18 @@ public class TQSExcelProcessor extends JFrame {
                 }
                 writer.write("  </Row>\n");
             }
-
+    
             writer.write("</Table>\n");
             writer.write("</Worksheet>\n");
             writer.write("</Workbook>\n");
-
+    
             outputArea.append("\nDados exportados para '" + file.getAbsolutePath() + "'");
         } catch (IOException e) {
             outputArea.append("\nErro ao salvar o arquivo XLS.");
         }
     }
+    
+    
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
